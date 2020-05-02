62245686811bd38aedf395b63e69e1fa0f046e93
Oleksii.Rozhkov <agroghkov@gmail.com>
2020-05-02 14:24:04 +0300

    fixes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const app = require('./app.js');
 
 const server = http.createServer(app);
-const port = process.env.APP_PORT;
+const port = 4000;
 
-server.listen(port);
+server.listen(port, '0.0.0.0', function() {
+  console.log('Listening on port %d', port);
+});
--- utils/configs.js
+++ utils/configs.js
@@ -7,9 +7,9 @@ const dbOptions = {
 
 module.exports.jwt_secret = 'ita-right.for.life_secret';
 module.exports.dbName = dbName;
-module.exports.dbURL = `mongodb://localhost:27017/${dbName}`;
-module.exports.animalsCollectionName = 'animals';
+// module.exports.dbURL = `mongodb://localhost:27017/right-for-life`;
+// module.exports.animalsCollectionName = 'animals';
 module.exports.serverPort = 4000;
-module.exports.frontURL = 'http://localhost:5000';
+// module.exports.frontURL = 'http://localhost:5000';
 module.exports.dbOptions = dbOptions;
-module.exports.connectionURI = `mongodb+srv://${process.argv[2]}:${process.argv[3]}@animalsforlife-goij5.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+module.exports.connectionURI = `mongodb+srv://root:root@animalsforlife-goij5.mongodb.net/${dbName}?retryWrites=true&w=majority`;
