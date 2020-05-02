39bb7fa7d8ede89dceafbb2e00d33dde1c3676c0
Oleksii.Rozhkov <agroghkov@gmail.com>
2020-05-02 14:59:16 +0300

    fixes

--- app.js
+++ app.js
@@ -29,16 +29,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //if use cors module --->>> app.use(cors())
-app.use(cors());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   if (req.method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use('/home', homeRoutes);
 app.use('/auth', authRoutes);
